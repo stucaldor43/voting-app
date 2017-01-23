71c7f42d7bf3cf8e7c4aee8ce58499d5908f082a
Anthony Cook <ddragonmaster49@yahoo.com>
2017-01-23 03:17:16 +0000

    Alter database schema

--- init_database.js
+++ init_database.js
@@ -12,8 +12,8 @@ const pool = new Pool({
 });
 
 const tableCreationStatements = [`CREATE TABLE Client ( id SERIAL PRIMARY KEY, name VARCHAR(64) NOT NULL UNIQUE);`,
-`CREATE TABLE Poll ( id SERIAL PRIMARY KEY, title VARCHAR(128) NOT NULL, fk_client_id INTEGER REFERENCES Client(id));`,
-`CREATE TABLE PollOptions ( id SERIAL PRIMARY KEY, message VARCHAR(256) NOT NULL, votes INTEGER NOT NULL DEFAULT 0, fk_poll_id INTEGER REFERENCES Poll(id));`,
-`CREATE TABLE Voter ( id SERIAL PRIMARY KEY, name VARCHAR(64) NOT NULL UNIQUE, fk_poll_id INTEGER REFERENCES Poll(id));`];
+`CREATE TABLE Poll ( id SERIAL PRIMARY KEY, title VARCHAR(128) NOT NULL, fk_client_id INTEGER NOT NULL REFERENCES Client(id));`,
+`CREATE TABLE PollOptions ( id SERIAL PRIMARY KEY, message VARCHAR(256) NOT NULL, votes INTEGER NOT NULL DEFAULT 0, fk_poll_id INTEGER NOT NULL REFERENCES Poll(id));`,
+`CREATE TABLE Voter ( id SERIAL PRIMARY KEY, name VARCHAR(64) NOT NULL UNIQUE, fk_poll_id INTEGER NOT NULL REFERENCES Poll(id));`];
 
 pool.query(tableCreationStatements.join(""));
