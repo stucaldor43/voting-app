73a8d1d7c960322e8664f8ee59ee6ea14c2915af
Anthony Cook <ddragonmaster49@yahoo.com>
2017-01-23 03:21:22 +0000

    Allow user to sign in using Twitter

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,9 +1,63 @@
 import React from 'react';
+import OAuth from 'oauth';
 
 class Header extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    
+    signIn() {
+      var self = this;
+      
+      var req = new XMLHttpRequest();
+      req.addEventListener("load", function() {
+        const response = JSON.parse(this.response);
+        if (response.status === "success") {
+          const data = response.data;
+          localStorage.setItem("token", data.token);
+          localStorage.setItem("secret", data.secret)
+          location.assign(`https://api.twitter.com/oauth/authorize?oauth_token=${localStorage.getItem("token")}`);
+        }
+        
+      });
+      req.open("GET", "/api/get_request_token");
+      req.send();
+    }
+    
+    componentDidMount() {
+      const queryString = location.search;
+      if (queryString.indexOf("oauth_token") >= 0 && 
+          queryString.indexOf("oauth_verifier") >= 0) {
+            const query = {};
+            const parameters = queryString.substring(1).split("&");
+            for (const parameter of parameters) {
+              query[parameter.split("=")[0]] = parameter.split("=")[1];
+            }
+            localStorage.setItem("verifier", query["oauth_verifier"]);
+      }
+      
+      var req = new XMLHttpRequest();
+      req.addEventListener("load", function() {
+        const response = JSON.parse(this.response);
+        if (response.status === "success") {
+          console.log(response);
+          
+        }
+        
+      });
+      req.open("GET", `/api/get_access_token?token=${localStorage.token}&secret=${localStorage.secret}&verifier=${localStorage.verifier}`);
+      req.send();
+    }
+    
     render() {
         return(<header>
-                
+                <nav>
+                  <ul>
+                    <li>
+                      <button onClick={this.signIn.bind(this)}>Sign In</button>
+                    </li>
+                  </ul>
+                </nav>
               </header>
         );    
     }
