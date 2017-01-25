97ef18f1f7342060051c09d65cfef470ec96007d
Anthony Cook <ddragonmaster49@yahoo.com>
2017-01-25 18:35:05 +0000

    Add error page

---
+++ app/components/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = (props) => {
+    return (
+      <div>
+        <h1>Error 404 - Page not found</h1>
+        <p>Sorry, the page you are trying to access is not available</p>
+      </div>
+    );
+};
+
+export default NotFound;
