055099591fdb2747a7acc18b09c89760f37d5312
Anthony Cook <ddragonmaster49@yahoo.com>
2017-01-27 02:55:07 +0000

    Add session middleware

--- server.js
+++ server.js
@@ -2,11 +2,23 @@ const express = require('express');
 const cons = require('consolidate');
 const routeManager = require('./route-manager.js');
 const environmentManager = require('./environment-manager.js');
+const session = require('express-session');
 
 environmentManager.setEnvironmentalVariables();
 
 const app = express();
 app.use('/', express.static(__dirname + '/public'));
+app.use('/', session({
+    cookie: {
+      httpOnly: false,
+      secure: false,
+      maxAge: null,
+      path: '/',
+    },
+    secret: process.env['SESSION_SECRET'],
+    resave: false,
+    saveUninitialized: false
+}));
 app.use('/api', (req, res, next) => {
     next();
 });
