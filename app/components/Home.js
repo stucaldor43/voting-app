ce03f60b813ad41bdc82920c1429bb95d6a11a9e
Anthony Cook <ddragonmaster49@yahoo.com>
2017-01-27 03:59:05 +0000

    Add methods to Home component

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,10 +1,30 @@
 import React from 'react';
 
 class Home extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {polls: []};
+    }
+    
+    componentDidMount() {
+        fetch("/api/polls")
+        .then(response => response.json())
+        .then(json => {
+            this.setState({polls: json.data});
+        });
+    }
+    
     render() {
         return (
             <div>
-            
+                {this.state.polls.map((poll) => {
+                    return (
+                        <div>
+                          <h1>{poll.title}</h1>
+                          <p>{poll.id}</p>
+                        </div>
+                    );
+                })}
             </div>
         );
     }
