ca46a68834e1820fbb8786749d055ed17bac578b
Anthony Cook <ddragonmaster49@yahoo.com>
2017-05-12 15:33:12 -0400

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+
+module.exports = {
+    entry: "./index.js",
+    output: {
+        path: path.resolve(__dirname, "public"),
+        filename: "bundle.js"
+    },
+    module: {
+      rules: [
+        {
+            test: /\.js$/,
+            exclude: path.resolve(__dirname, "node_modules"),
+            use: {
+                loader: "babel-loader",
+                options: {
+                    presets: ["es2015"]
+                }
+            }
+        }    
+      ]  
+    },
+    devtool: "source-map",
+    plugins: [
+        new BrowserSyncPlugin({
+            host: "localhost",
+            port: 8080,
+            proxy: "localhost:8081"
+        })
+    ]
+};
