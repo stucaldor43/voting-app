f2ea6a8dd8c6ce2fa530373fd040c50d2011df38
Anthony Cook <ddragonmaster49@yahoo.com>
2017-05-28 15:33:23 -0400

    Add Piechart component

---
+++ components/PieChart.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { PropTypes } from 'react';
+import Chart from 'chart.js';
+
+class PieChart extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    
+    componentDidMount() {
+        this.drawChart();
+    }
+    
+    componentDidUpdate(prevProps, prevState) {
+        this.drawChart();
+    }
+    
+    drawChart() {
+        const ctx = document.getElementById("chart");
+        const pieChart = new Chart(ctx, {
+           type: "pie",
+           data: {
+               labels: this.props.chartLabels,
+               datasets: [{
+                    data: this.props.chartData,
+                    backgroundColor: [
+                        "#50C7FB",
+                        "#FB50C7",
+                        "#C7FB50"
+                    ],
+                    hoverBackgroundColor: [
+                        "#50C7FB",
+                        "#FB50C7",
+                        "#C7FB50"    
+                    ]
+               }]
+           }
+        });
+    }
+    
+    render() {
+        return (
+          <canvas id="chart" width="500" height="500"></canvas>  
+        );
+    }
+}
+
+export default PieChart;
